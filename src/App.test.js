4827aba532875beaa729e970e7eaff36190ba9e2
Savio A <savio.abuga@andela.com>
2019-07-01 11:22:07 +0300

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render( < App / > , div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render( < App / > , div);
   ReactDOM.unmountComponentAtNode(div);
 });
